241e351264340bef8de07019943057027713fd37
Vento <github@brenneisen.me>
2024-06-08 20:01:54 +0200

    Update some packages

---
+++ src/views/BrowserLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <a :href="uri" target="_blank"><slot></slot></a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BrowserLink',
+  props: {
+    uri: String
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
